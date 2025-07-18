75e6a3c420cde06bdb87cdc65da361cc5cd841c3
Sakshi-0795 <moresakshi796@gmail.com>
2025-07-18 20:31:00 +0530

    Add files via upload

---
+++ aithmatic.java
@@ -0,0 +1,26 @@
+public class aithmatic{
+public static void main(String[]args){
+    int a,b,c;
+    a=50;
+    b=60;
+    c=a+b;
+    System.out.println("Addition="+c);
+
+    a=12;
+    b=13;
+    c=a-b;
+    System.out.println("Subtraction="+c);
+                                           
+    a=34;
+    b=45;
+    c=a*b;
+    System.out.println("multiplication="+c);
+
+    a=33;
+    b=65;
+    c=a/b;
+    System.out.println("division="+c);
+}
+
+
+}
